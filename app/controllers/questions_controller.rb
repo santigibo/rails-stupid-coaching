20db485944b88e331d73cbb0efb8fa090a709866
Santiago Giraldo <santigibo@hotmail.com>
2020-02-17 11:37:02 -0500

    Created the ask and answer pages to display and take the user question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,16 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I'm going to work"
+      @answer = 'Great'
+    elsif !@question.nil? && @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    @question = "" if @question.nil?
+  end
 end
